fcbe9536325fb87b464f02a0166d2bf0655126b7
Levin <levin70@outlook.com>
2022-09-17 23:18:43 +0900

    дз, семинар 1, задача 8

---
+++ DZ_8_Seminar_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Запросить число N  и вывести на экран все четные числа от 1 до N
+Console.Clear();
+Console.Write("Введите положительно число больше 1 ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num <= 0) Console.WriteLine("Вы ввели отрицательное число, попробуйте снова");
+if(num == 1) Console.WriteLine("Вы ввели 1, попробуйте снова");
+int sch = 2;
+while(sch <= num)
+{
+    Console.Write(sch);
+    Console.Write(",");
+    sch = sch + 2;
+}
+
