4c370828cc6a89849f181af0cc036a8e93e60e35
Levin <levin70@outlook.com>
2022-09-17 19:56:05 +0900

    Семинар 1, пример 4

---
+++ ExSe_4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Запрос ввода трехзначного числа
+// вывод на экран последней цифры этого числа
+Console.Clear();
+Console.Write("Введите трехзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if((num > 99) & (num < 1000))
+{
+    int znak3 = num % 10;
+    Console.Write(znak3);
+}
+else
+{
+    Console.WriteLine("Введено неверное число. Попробуй еще раз.");
+}
+   
