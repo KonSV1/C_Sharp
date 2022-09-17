7019e17c1dbd50eea2898fec7375a3faff81098c
Levin <levin70@outlook.com>
2022-09-17 18:55:52 +0900

    Семинар 1, пример 3

--- ExSe_3/Program.cs
+++ ExSe_3/Program.cs
@@ -4,3 +4,10 @@ Console.Clear();
 Console.Write("Введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
 int numOtr = num * -1;
+int count = numOtr;
+
+while(count <= num)
+{
+    Console.Write($"{count}, ");
+    count +=1;
+}
