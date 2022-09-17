341027d4081b0ab0cbfc57b3cfca2811b2de5bbc
Levin <levin70@outlook.com>
2022-09-17 21:09:06 +0900

    маша

--- Example_005/Program.cs
+++ Example_005/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
-if (username.ToLower() == "маша") Console.WriteLine("Ура, это же Маша!");
+if(username.ToLower() == "маша") Console.WriteLine("Ура, это же Маша!");
 else
 {
      Console.Write("Привет, ");
