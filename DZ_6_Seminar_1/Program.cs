a14a02da335b891989bf91cb75d1c6685d0539f9
Levin <levin70@outlook.com>
2022-09-17 23:26:31 +0900

    дз, семинар 1, задача 6

---
+++ DZ_6_Seminar_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу запроса числа и вывода на экран является ли оно четным или нет.
+
+Console.Clear();
+Console.Write("Введите целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0) Console.WriteLine($"Число {num} является четным");
+else
+{
+    Console.WriteLine($"Число {num} не является четным");
+}
