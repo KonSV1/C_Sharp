fd7100527a9db6e5cf50a1a65e233d2f5afe99f4
Levin <levin70@outlook.com>
2022-09-17 22:30:57 +0900

    дз, семинар 1, задача 4

---
+++ DZ_4_Seminar_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу запроса 3-х чисел
+// и вывода на экран максимального из них.
+
+Console.Clear();
+Console.Write("Введите целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0) Console.WriteLine($"Число {num} является четным");
+else
+{
+    Console.WriteLine($"Число {num} не является четным");
+}
