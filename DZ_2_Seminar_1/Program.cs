5e89ad4a22d9fb4089c9b161eb8a853e3d93e119
Levin <levin70@outlook.com>
2022-09-17 22:12:39 +0900

    семинар 1, задача 2

---
+++ DZ_2_Seminar_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу запроса 2-х чисел
+// и вывода на экран какое больше, а какое меньше
+
+Console.Clear();
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2) Console.WriteLine($"число {num1} больше числа {num2}");
+else
+{
+    Console.WriteLine($"число {num1} меньше числа {num2}");
+}
