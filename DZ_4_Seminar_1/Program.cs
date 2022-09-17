17743ae8f23e8ef57172f645b93d339ec38e1adc
Levin <levin70@outlook.com>
2022-09-17 23:34:44 +0900

    дз, семинар 1, задача 4

--- DZ_4_Seminar_1/Program.cs
+++ DZ_4_Seminar_1/Program.cs
@@ -1,10 +1,14 @@
-﻿// Написать программу запроса числа и вывода на экран является ли оно четным или нет.
+﻿// Написать программу запроса 3-х чисел
+// и вывода на экран максимального из них.
 
 Console.Clear();
-Console.Write("Введите целое число ");
-int num = Convert.ToInt32(Console.ReadLine());
-if(num % 2 == 0) Console.WriteLine($"Число {num} является четным");
-else
-{
-    Console.WriteLine($"Число {num} не является четным");
-}
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+Console.WriteLine($"Из чисел {num1}, {num2}, {num3} наибольшее число {max}");
