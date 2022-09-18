e731721e8f7c3f456826e23c4e68743be088ba3a
Levin <levin70@outlook.com>
2022-09-18 12:42:10 +0900

    лекция 2, пример 3, поиск элемента

---
+++ Example_003_L2/Program.cs
@@ -0,0 +1,17 @@
+﻿// найти элемент массива из N элементов соответсвующий значению find
+Console.Clear();
+//      имя     0  1   2    3    4   5    6    7 
+int[] arrey = {23, 18, 85, 243, 186, 65, 18, 263, -3, 18}; // создание массива
+int n = arrey.Length;
+// Console.WriteLine(n);
+int find = 243;
+int index = 0;
+while (index < n)
+{
+    if(arrey[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; //index = index + 1
+}
