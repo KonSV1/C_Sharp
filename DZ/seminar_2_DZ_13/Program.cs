2e73e5259de64e0c7f636608a2555dc77de65f69
Levin <levin70@outlook.com>
2022-09-19 00:05:12 +0900

    ДЗ Семинар 2, задача 13

--- DZ/seminar_2_DZ_13/Program.cs
+++ DZ/seminar_2_DZ_13/Program.cs
@@ -4,12 +4,21 @@
 //Console.Clear();
 Console.Write("Введите число  ");
 int num = Convert.ToInt32(Console.ReadLine());
-if((num > 99) & (num < 1000))
+int num1 = num;
+int num2;
+if(num > 99)
 {
-    int num1 = num % 10;
-    Console.Write($"{num} -> {num1}");
+    if (num > 1000)
+    {
+       while (num > 1000)
+        {
+            num = num / 10;
+        }   
+    }
+    num2 = num % 10;
+    Console.Write($"{num1} -> {num2}");
 }
 else
 {
-    Console.Write("Введенное число не содержит третей цифры"); 
+   Console.Write("Введенное число не содержит третей цифры");
 }
