11cc672350ef2925f4c0dcc02b215b28ad47a6bc
Levin <levin70@outlook.com>
2022-09-18 17:59:50 +0900

    Семинар 1, задача 3

---
+++ Ex1Se_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Запросить число "N", вывести на экра все целые числа от "-N" до "N"
+Console.Clear();
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count;
+if (num > 0) count = -num;
+else
+{
+    count = num;
+    num = -num;
+}
+    while (count <= num)
+    {
+        Console.Write($"{count}, ");
+        count += 1;
+    }
