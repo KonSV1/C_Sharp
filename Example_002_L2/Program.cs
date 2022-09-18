689e755621b1a303875d45ad84b37245c7fe5dba
Levin <levin70@outlook.com>
2022-09-18 12:03:33 +0900

    лекция 2, пример 2, массив

---
+++ Example_002_L2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Создаем массивы
+
+//arrey [0] = 5;   // запись значения в массив (значение "5" в индекс "0")
+//Console.WriteLine(arrey[0]); // вывод на экран элемента массива с индексом "4"
+
+Console.Clear();
+int Max(int arg1, int arg2, int arg3)
+{
+    int res = arg1;
+    if(arg2 > res) res = arg2;
+    if(arg3 > res) res = arg3;
+    return res;
+}
+//      имя     0  1   2    3    4   5    6    7  8
+int[] arrey = {23, 18, 85, 243, 186, 65, 263, -3, 97}; // создание массива
+int max = Max(Max(arrey[0], arrey[1], arrey[2]),
+Max(arrey[3], arrey[4], arrey[5]),
+Max(arrey[6], arrey[7], arrey[8]));
+Console.WriteLine(max);
