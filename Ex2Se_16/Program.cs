06489f6ea7bf92216b040c610773bcc267aedcd1
Levin <levin70@outlook.com>
2022-09-18 19:46:11 +0900

    Семинар 2, пример 16

---
+++ Ex2Se_16/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли одно из них квадратом другого
+//5, 25 -> Да
+//16, 5 -> Нет
+
+Console.Clear();
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if((num1 * num1 == num2) || (num2 * num2 == num1))
+{
+    Console.Write($"{num1}, {num2} - Да");
+}
+else
+{
+   Console.Write($"{num1}, {num2} - Нет"); 
+}
