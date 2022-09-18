e13ee4f37dc58d11b4708fe0c84419fd3530a5bc
Levin <levin70@outlook.com>
2022-09-18 22:13:37 +0900

    ДЗ Семинар 2, задача 13

---
+++ DZ/seminar_2_DZ_13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13 Напишите программу, которая сообщает третью цифру
+//заданного числа или сообщает, что ее нет.
+
+//Console.Clear();
+Console.Write("Введите число  ");
+int num = Convert.ToInt32(Console.ReadLine());
+if((num > 99) & (num < 1000))
+{
+    int num1 = num % 10;
+    Console.Write($"{num} -> {num1}");
+}
+else
+{
+    Console.Write("Введенное число не содержит третей цифры"); 
+}
