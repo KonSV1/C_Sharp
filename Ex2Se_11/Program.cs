ad2fef2f1a83bc02e73975fd32a459aa8ff2c2de
Levin <levin70@outlook.com>
2022-09-18 16:57:56 +0900

    Семинар 2, пример 11

---
+++ Ex2Se_11/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+Console.Clear();
+int num = new Random().Next(99, 1000);
+Console.WriteLine(num);
+int num1 = num /100;
+//Console.Write(num1);
+int num2 = num % 10;
+//Console.Write(num2);
+Console.WriteLine($"{num1}{num2}");
