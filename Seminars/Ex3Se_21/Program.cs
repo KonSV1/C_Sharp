0cc943b24aa049ac9b4ab6581195b1c3ad8aba6a
Levin <levin70@outlook.com>
2022-09-25 00:38:36 +0900

    Семинар 3, пример 21

--- Seminars/Ex3Se_21/Program.cs
+++ Seminars/Ex3Se_21/Program.cs
@@ -6,15 +6,11 @@
 Console.Clear();
 Console.Write("Введите координату х для первой точки ");
 int X = Convert.ToInt32(Console.ReadLine());
-//X = Math.Abs(X);
 Console.Write("Введите координату y для первой точки ");
 int Y = Convert.ToInt32(Console.ReadLine());
-//Y = Math.Abs(Y);
 Console.Write("Введите координату х для второй точки ");
 int X1 = Convert.ToInt32(Console.ReadLine());;
-//X1 = Math.Abs(X1);
 Console.Write("Введите координату y для второй точки ");
 int Y1 = Convert.ToInt32(Console.ReadLine());
-//Y1 = Math.Abs(Y1);
 double C = Math.Round(Math.Sqrt((Math.Pow((X1-X), 2) + Math.Pow((Y1-Y), 2))), 2);
-Console.WriteLine(C);
+Console.WriteLine($"A ({X}, {Y}); B ({X1}, {Y1}) -> {C}");
