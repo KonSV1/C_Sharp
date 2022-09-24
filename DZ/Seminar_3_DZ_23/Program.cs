303c2871235a5cfa1cc0bd5bfd72edee24fd750a
Levin <levin70@outlook.com>
2022-09-25 01:15:40 +0900

    ДЗ 3, пример 23

---
+++ DZ/Seminar_3_DZ_23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int N1 = Math.Abs(N);
+int[] arrey = new int[N1];
+Console.Write($"{N} -> ");
+int i1 = 0;
+for(int i = 0; i < N1; i++)
+{
+    if(N > 0)
+    {
+        arrey[i] = i+1;
+        int Tr = Convert.ToInt32(Math.Pow(arrey[i], 3));
+        Console.Write($"{Tr}, ");
+    }
+    else
+    {
+        arrey[i] = i1-1;
+        i1--;
+        int Tr = Convert.ToInt32(Math.Pow(arrey[i], 3));
+        Console.Write($"{Tr}, ");   
+    }
+}
