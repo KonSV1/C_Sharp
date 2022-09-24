9f5dbb3f929b688c54d038cc57f94a7514dfd716
Levin <levin70@outlook.com>
2022-09-24 21:26:19 +0900

    Семинар 3, примеры 18_21_22

---
+++ Seminars/Ex3Se_21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.Clear();
+Console.Write("Введите координату х для первой точки ");
+int X = Convert.ToInt32(Console.ReadLine());
+//X = Math.Abs(X);
+Console.Write("Введите координату y для первой точки ");
+int Y = Convert.ToInt32(Console.ReadLine());
+//Y = Math.Abs(Y);
+Console.Write("Введите координату х для второй точки ");
+int X1 = Convert.ToInt32(Console.ReadLine());;
+//X1 = Math.Abs(X1);
+Console.Write("Введите координату y для второй точки ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+//Y1 = Math.Abs(Y1);
+double C = Math.Round(Math.Sqrt((Math.Pow((X1-X), 2) + Math.Pow((Y1-Y), 2))), 2);
+Console.WriteLine(C);
---
+++ Seminars/Ex3Se_22/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 22: Напишите программу, которая принимает на вход число (N)
+//и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arrey = new int[N];
+Console.Write($"{N} -> ");
+for(int i = 0; i < N; i++)
+{
+     arrey[i] = i+1;
+     int sqw = Convert.ToInt32(Math.Pow(arrey[i], 2));
+     Console.Write($"{sqw}, ");
+}
