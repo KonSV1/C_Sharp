6c55861e78826219a9975e01ef08b24365b48345
Levin <levin70@outlook.com>
2022-09-27 00:20:15 +0900

    Семинар 4

---
+++ DZ/Seminar_4_DZ_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+double r = (Math.Floor(Math.Log10(num) + 1));   // Определяем количество разрядов в числе
+int p;                                          // Вводим переменую для взятия остатка от / на 10
+int num1 = num;                                 // надо сохранинить начальное значение num для этого вводим num1
+int sum = 0;
+for(int i = 0; i < r; i++)
+{
+    p = num1 % 10;
+    num1 = num1 / 10;
+    sum = sum +p;
+}
+Console.WriteLine($"{num} -> {sum}");
---
+++ DZ/Seminar_4_DZ_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int[] arrey = new int[8];
+for(int i = 0; i < 7; i++)
+{
+   arrey[i] = new Random().Next(0, 100);
+   Console.Write($"{arrey[i]}, ");
+}
+arrey[7] = new Random().Next(0, 100);
+Console.Write($"{arrey[7]}");
+Console.Write(" -> [");
+for(int i = 0; i < 7; i++)
+{
+   Console.Write($"{arrey[i]}, ");
+}
+Console.WriteLine($"{arrey[7]}]");
