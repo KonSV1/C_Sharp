3f16934bda6a100cc5cd4d79af3a7708d96f165b
Levin <levin70@outlook.com>
2022-10-03 22:29:08 +0700

    лекция 4

---
+++ Lectures/L4_Example_001/Program.cs
@@ -0,0 +1,61 @@
+﻿// Двумерные массивы
+// string[,] table = new string[2,5];   // Создание массива из 2-х строк и 5-ти столбцов
+
+
+// table[1,2] = "Слово";
+
+// for (int rows = 0; rows < 2; rows ++)
+// {
+//     for (int colums = 0; colums < 5; colums++)
+//     {
+//         Console.WriteLine($"{table[rows, colums]} -");
+//     }
+// }
+
+// int[,] matrix = new int[3, 4];
+
+// for (int i = 0; i < matrix.GetLength(0); i ++)      // GetLength(0) - определяет сколько в массиве строк
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)   // GetLength(1) - определяет сколько в массиве столбцов
+//     {
+//         Console.Write($"{matrix[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// применение методов
+
+
+
+
+void PrintArrey(int[,] matr)        // метод вывода на экран массива
+{
+    for (int i = 0; i < matr.GetLength(0); i ++)     
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) 
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillArrey(int[,] matr)         //метод заполнения массива ПСП
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+int[,] matrix = new int[3, 4];
+PrintArrey(matrix);
+Console.WriteLine();
+FillArrey(matrix);
+PrintArrey(matrix);
---
+++ Lectures/L4_Example_003/Program.cs
@@ -0,0 +1,31 @@
+﻿// Рекурсии
+
+// Вычисление факториала
+// double Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n* Factorial(n-1);
+// }
+// for(int i = 1; i<30; i++)
+// {
+// Console.WriteLine($"{i}! = {Factorial(i)}");
+//}
+
+// Числа Фибоначчи
+//f(1) = 1
+//f(2) = 1
+//f(3) = 2
+//f(4) = 3
+//f(5) = 5
+//f(n) = f(n-1) + f(n-2)
+
+ int Fibonacci(int n)
+ {
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+ }
+
+for (int i = 1; i < 60; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
