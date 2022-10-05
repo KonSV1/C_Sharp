1d137a47bdeed6a0007986d38e060c9e442d7f9f
Levin <levin70@outlook.com>
2022-10-05 21:44:59 +0700

    DZ_Seminar_06

---
+++ DZ/S6/DZ_41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41
+//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Сколько чисел желаете ввести? ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[] arrey = new double[m];
+int a = 0;
+for (int i = 0; i < m; i++)
+{
+    Console.Clear();
+    Console.Write($"Введите число № {i + 1}  ");
+    arrey[i] = Convert.ToDouble(Console.ReadLine());
+    if (arrey[i] > 0) a++;
+}
+Console.Clear();
+PrintArrey(arrey);
+
+
+
+
+
+
+
+void PrintArrey(double[] arr) // Метод вывода результата
+{
+    for(int i = 0; i < m - 1; i++)
+    {
+        Console.Write($"{arrey[i]}, ");
+    }
+    Console.Write($"{arrey[m-1]} -> {a} ");
+}
