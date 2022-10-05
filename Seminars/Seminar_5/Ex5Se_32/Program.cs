0d0ee56df6d2ffa6e5dc9d085a6ed8dbc067d350
Levin <levin70@outlook.com>
2022-10-05 21:42:02 +0700

    Семинар 5

---
+++ Seminars/Seminar_5/Ex5Se_32/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 32.
+//Напишите программу замена элементов массива:
+//положительные заментиь на соответсвующиме отрицательные и наоборот.
+Console.Clear();
+int n = new Random().Next(5, 10);
+int[] arrey = new int[n];
+Console.Write($"[");
+for(int i = 0; i < n-1; i++)
+{
+    arrey[i] = new Random().Next(-9, 10);
+    Console.Write($"{arrey[i]}, ");
+}
+ Console.Write($"{arrey[n-1]}]");
+NegativeArrey();
+
+
+void NegativeArrey() // Метод замены и вывода на экран
+{
+    Console.Write($"  ->  [");
+    for(int i = 0; i < n-1; i++)
+    {
+        arrey[i] = arrey[i] * -1;
+        Console.Write($"{arrey[i]}, ");
+    }
+    Console.Write($"{arrey[n-1]}]");
+}
