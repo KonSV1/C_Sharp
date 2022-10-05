93e3a2c8e95c26873e59ccefda819bef7c94d53c
Levin <levin70@outlook.com>
2022-10-05 10:16:16 +0700

    Лекция_04

--- Lectures/Lectures_04/Example_003/Program.cs
+++ Lectures/Lectures_04/Example_003/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Рекурсии
 
 // Вычисление факториала
-// double Factorial(int n)
-// {
-//     if (n == 1) return 1;
-//     else return n* Factorial(n-1);
-// }
-// for(int i = 1; i<30; i++)
-// {
-// Console.WriteLine($"{i}! = {Factorial(i)}");
-//}
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n* Factorial(n-1);
+}
+for(int i = 1; i<30; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
 
 // Числа Фибоначчи
 //f(1) = 1
