5834d1aa979eae9c62f42697ed0e5ed9f691a06b
Levin <levin70@outlook.com>
2022-10-06 15:29:46 +0700

    DZ_Seminar_06

--- DZ/S6/DZ_43/Program.cs
+++ DZ/S6/DZ_43/Program.cs
@@ -3,7 +3,7 @@
 //y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-Console.Clear();
+//Console.Clear();
 Console.WriteLine("Введите K1  ");
 double k1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите K2  ");
@@ -20,6 +20,6 @@ else
 {
     double x = (b2-b1)/(k1-k2);
     double y = k1*x +b1;
-    Console.Clear();
+    //Console.Clear();
     Console.Write($"B1 = {b1}, K1 = {k1}, B2 = {b2}, K2 = {k2}  ->  ({x}; {y})");
 }
