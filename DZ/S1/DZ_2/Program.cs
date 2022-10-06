e935ca1419613b4508f0a103db4f46ab1e485c9c
Levin <levin70@outlook.com>
2022-10-06 15:31:41 +0700

    лекция 5

--- DZ/S1/DZ_2/Program.cs
+++ DZ/S1/DZ_2/Program.cs
@@ -6,7 +6,7 @@ Console.Write("Введите первое число ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-if(num1 > num2) Console.WriteLine($"число {num1} больше числа {num2}");
+if (num1 > num2) Console.WriteLine($"число {num1} больше числа {num2}");
 else
 {
     Console.WriteLine($"Число {num1} меньше числа {num2}");
--- DZ/S2/DZ_15/Program.cs
+++ DZ/S2/DZ_15/Program.cs
@@ -5,15 +5,15 @@
 Console.Clear();
 Console.Write("Введите число от 1 до 7  ");
 int num = Convert.ToInt32(Console.ReadLine());
-if((num > 0) & (num < 8))
+if ((num > 0) & (num < 8))
 {
-    if ((num == 6) || (num ==7))
+    if ((num == 6) || (num == 7))
     {
         Console.Write($"{num} -> Да");
     }
     else
     {
-         Console.Write($"{num} -> Нет");
+        Console.Write($"{num} -> Нет");
     }
 }
 else
