74281d71f9e612ad814961277fbc874cb04b55fc
Levin <levin70@outlook.com>
2022-10-06 20:53:50 +0700

    Лекция 5

--- Lectures/Lectures_05/Program.cs
+++ Lectures/Lectures_05/Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-// int a=0; int b=1; int c=3; int e=4; int r=0; int f=6;
-// if ((a > b
-//     && c < e
-//     && r == e
-//     || c == f));
-// {
-//     Console.WriteLine();
+int a=0; int b=1; int c=3; int e=4; int r=0; int f=6;
+if ((a > b
+    && c < e
+    && r == e
+    || c == f));
+{
+    Console.WriteLine();
+}
