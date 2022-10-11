9e8a8787114ed5c4de3eace3600aebedb57f9ee5
Levin <levin70@outlook.com>
2022-10-11 16:12:51 +0700

    Доработка_60 под уточненные условия

--- DZ/S8/DZ_60/Program.cs
+++ DZ/S8/DZ_60/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Сформируйте трехмерный массив из неповторяющихся двухзначных чисел.
 //Напишите программу, которая будет построчно выводить массив добавляя индексы каждого элемента
 
-//Console.Clear();
-int[,,] arrey = new int[4, 4, 5];
+Console.Clear();
+int[,,] arrey = new int[2, 2, 2];
 int n = 0;
 int[] num = new int[arrey.GetLength(0) * arrey.GetLength(1) * arrey.GetLength(2)];
 FillMonoArrey(num);
@@ -12,8 +12,8 @@ for (int i = 0; i < arrey.GetLength(0); i++)
     {
         for (int k = 0; k < arrey.GetLength(2); k++)
         {
-            arrey[i, j, k] = num[n];
-            Console.Write($"{arrey[i, j, k]}  index - [{i}, {j}, {k}]   ");
+            arrey[i, k, j] = num[n];
+            Console.Write($"{arrey[i, k, j]}  index - [{i}, {k}, {j}]   ");
             n++;
         }
         Console.WriteLine();
@@ -24,12 +24,13 @@ for (int i = 0; i < arrey.GetLength(0); i++)
 
 void FillMonoArrey(int[] arr)
 {
-    for (int i = 0; i < arr.Length; i++)
+    arr[0] = new Random().Next(10, 100);
+    for (int i = 1; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(10, 100);
-        for (int j = 0; j <= i; j++)
+        for (int j = 0; j < i; j++)
         {
-            if(arr[i] == arr[j]) arr[i] = new Random().Next(10, 100);
+        Flag: arr[i] = new Random().Next(10, 100) + 1;
+            if (arr[i] == arr[j]) goto Flag;
         }
     }
 }
