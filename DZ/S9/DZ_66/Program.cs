a1ee78f9b492ee595798d28f2c4ec208ebbdd301
Levin <levin70@outlook.com>
2022-10-16 18:47:12 +0700

    DZ_Seminar_9

---
+++ DZ/S9/DZ_66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+//Console.Clear();
+Console.Write($" Введите положительное число  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($" Введите еще одно положительное число  ");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((m < 0) || (n < 0))
+{
+    Console.Write("Одно или оба числа отрицательные. Попробуй еще раз");
+}
+else
+{
+    if (n < m)
+    {
+        (m, n) = (n, m);
+    }
+    Console.Write($"M = {m}; N = {n} -> ");
+    Console.Write(Print(m));
+
+
+    int Print(int arg)
+    {
+        if (arg <= n) return arg + Print(arg + 1);
+        return 0;
+    }
+}
---
+++ DZ/S9/DZ_68/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+Console.Write($" Введите первое число функци Аккермана  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($" Введите второе число функци Аккермана  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"m = {m}, n = {n} -> A(m, n) = {Akkerman(m,n)}");
+
+
+int Akkerman(int arg1, int arg2)
+{
+    if (arg1 == 0) return arg2 + 1;
+    else
+    {
+        if ((arg1 > 0) && (arg2 == 0)) return Akkerman(arg1 - 1, 1);
+        else
+        {
+            return Akkerman(arg1 - 1, Akkerman(arg1, arg2 - 1));
+        }
+    }
+}
